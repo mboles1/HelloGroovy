c35fe7517042cab71631342283d5fbabc534ecd4
Matt Boles <mooman214@gmail.com>
2014-01-18 10:57:33 -0500

    First Commit

---
+++ src/hellogroovy/HelloGroovyWorld.groovy
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hellogroovy
+
+/**
+ *
+ * @author matt_boles
+ */
+class HelloGroovyWorld 
+{
+	public static void main(String[] args)
+        {
+            println("Hello Groovy");
+        }
+}
+
